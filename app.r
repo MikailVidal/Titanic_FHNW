cc9517473799dea26cea34bbd3f199d0c8973b7b
Elias Pulver <91259212+ibidreli@users.noreply.github.com>
2024-09-12 17:31:16 +0200

    Updated

--- app.r
+++ app.r
@@ -26,6 +26,28 @@ passengers$Age[passengers$Age > 0 & passengers$Age < 1] <- 1
 # Delete entries where the age is under 0 or the amount of family members surpasses 20
 passengers <- passengers %>% filter(!(Age < 0 | FamilyMembers > 20))
 
+
+# Verteilung der Todesopfer und überlebenden nach Alter
 ggplot(passengers, aes(x = Age, fill = factor(Survived))) +
   geom_histogram(binwidth = 5, position = "dodge") +
-  labs(x = "Alter", y = "Anzahl") 
+  labs(x = "Alter", y = "Anzahl") +
+  scale_fill_manual(values = c("#A70709", "green"), labels = c("Nicht Überlebt", "Überlebt"))
+
+# Warscheindlichkeit für Tod bei 25 Jährigen
+filtered_data <- subset(passengers, Age == 25)
+ggplot(filtered_data, aes(x = Age, fill = factor(Survived))) +
+  geom_histogram(binwidth = 1, position = "dodge") +
+  labs(x = "Alter", y = "Anzahl") +
+  scale_fill_manual(values = c("#A70709", "green"), labels = c("Nicht Überlebt", "Überlebt"))
+
+
+
+# Analyse ob Daten der Passagier Tickets ähndlichkeiten haben -> alle sitzen in der gleichen Klasse 
+passengers$Ticket <- as.character(passengers$Ticket)
+
+ticket_list <- c("19928", "19950", "PC 17755", "110152", "19929")
+
+ticket_numbers <- subset(passengers, Ticket %in% ticket_list)
+print(ticket_numbers)
+
+

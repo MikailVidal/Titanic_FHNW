4058b77fa51958ed633739e4e1ea590479f56877
MikailVidal <mikail.vidal@students.fhnw.ch>
2024-09-12 09:38:55 +0200

    Change file name of read.csv, add arguments

--- app.r
+++ app.r
@@ -6,12 +6,13 @@ library(rpart)
 library(rpart.plot)
 
 
-setwd("/Users/mikailvidal/Projects/r_tutorial")
+setwd("/Users/mikailvidal/DataspellProjects/Titanic_FHNW")
 
-passengers <- read.csv("minichallenge_titanic-1661852450_modified-1726068537.csv", sep = ";")
+passengers <- read.csv("Mini-Challenge_Titanic.csv", sep = ";")
 
 
 # Group and filter passengers by age and survival
 print("Group by")
 print(passengers %>% group_by(Age) %>% summarise(survived = sum(Survived), .groups = 'drop'), n = 90)
 
+passengers %>% summary()
